601a48d46d51c5f697d18467e2c7f607763bf55b
Georgii Turevich <georgy.turevich@gmail.com>
2025-05-13 12:08:53 +0700

    Use get_user_orders in performance tests

--- tests/kuruapi/test_http_kuruapi_performance.py
+++ tests/kuruapi/test_http_kuruapi_performance.py
@@ -10,12 +10,14 @@ async def test_compare_performance():
     load_dotenv()
 
     api = KuruAPI(url=os.getenv("KURU_API_URL"))
-    orders_response = api.get_active_orders(os.getenv("USER_ADDRESS"))
+    orders_response = api.get_user_orders(os.getenv("USER_ADDRESS"), limit=3)
     assert len(orders_response) > 0
+    print(f"\nUser orders: {orders_response}")
 
     api_fork = KuruAPIFork(url=os.getenv("KURU_API_URL"))
-    orders_response_fork = await api_fork.get_active_orders(os.getenv("USER_ADDRESS"))
+    orders_response_fork = await api_fork.get_user_orders(os.getenv("USER_ADDRESS"), limit=3)
     assert len(orders_response_fork) > 0
+    print(f"\nUser orders from fork: {orders_response_fork}")
 
 
 
